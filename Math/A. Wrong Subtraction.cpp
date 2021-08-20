5763489cb4d695262aae5fbf0f56e71803ad6161
Aminul Islam <72270613+amine116@users.noreply.github.com>
2021-08-20 18:02:01 +0600

    cf

---
+++ Math/A. Wrong Subtraction.cpp
@@ -0,0 +1,70 @@
+/*
+    Author: Aminul Islam.
+    Email: iaminul237@gmail.com
+    Facebook: www.facebook.com/aminul.islam116
+    Department: mathematics
+    Institution: Shahjalal university of science and technology, Sylhet.
+    Bangladesh.
+*/
+#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define sll signed long long
+#define I() ( { int a ; read(a) ; a; } )
+#define L() ( { ll a ; read(a) ; a; } )
+#define D() ({double a; scanf("%lf", &a); a;})
+#define UL() ( { ull a; read(a); a; } )
+#define SL() ( { sll a; read(a); a; } )
+#define getString() ({ string s; cin >> s; s;})
+#define mapLL map<ll, ll>
+#define mapSL map<string, ll>
+#define mapLLIt mapLL::iterator
+#define mapSLIt mapSL::iterator
+
+#define rep(n) for(ll i = 0; i < n; i++)
+#define repMapLL(mp) for(mapLLIt it = mp.begin(); it != mp.end(); it++)
+#define repMapSL(mp) for(mapSLIt it = mp.begin(); it != mp.end(); it++)
+
+#define print1(a) (printf("%I64d ", a))
+#define print1ln(a) (printf("%I64d\n", a))
+#define print2(a, b) (printf("%I64d %I64d ", a, b))
+#define print2ln(a, b) (printf("%I64d %I64d\n", a, b))
+#define print3ln(a, b, c) (printf("%I64d %I64d %I64d\n", a, b, c))
+#define newLine (printf("\n"))
+
+#define mod 1000000007
+#define MAX 300005
+
+template<class T>inline bool read(T &x){
+    int c=getchar();int sgn=1;
+    while(~c&&c<'0'|c>'9'){if(c=='-')sgn=-1;c=getchar();}
+    for(x=0;~c&&'0'<=c&&c<='9';c=getchar())x=x*10+c-'0';
+    x*=sgn; return ~c;
+}
+using namespace std;
+
+ll getPower(ll x, ll p){
+    ll power = x;
+    if(x == 0) return 0;
+    if(p == 0) return 1;
+
+    for(ll i = 2; i <= p; i++){
+        power *= x;
+    }
+    return power;
+
+}
+///---------------------endOfSnippet----------------|o|
+
+int main(){
+
+    int n = I(), k = I(), res = 0;
+
+    while(k--){
+        if(n%10 == 0) n /= 10;
+        else n--;
+    }
+
+    printf("%d", n);
+    return 0;
+}
